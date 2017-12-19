2d85e4b16adf3563a2e420ca6a496c714154614c
rsquaredin <rsquaredin@gmail.com>
2017-12-19 23:29:14 +0530

    package file

---
+++ R/blr-blorr.R
@@ -0,0 +1,13 @@
+#' \code{blorr} package
+#'
+#' Tools for developing binary logistic regression models
+#'
+#' See the README on
+#' \href{https://github.com/rsquaredacademy/blorr}{GitHub}
+#'
+#' @docType package
+#' @name blorr
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
