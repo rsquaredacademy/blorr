7bcfd7a30ecd81354ff27db339caf75ab622cbbb
rsquaredin <rsquaredin@gmail.com>
2017-12-28 19:39:56 +0530

    add global variables

--- R/blr-blorr.R
+++ R/blr-blorr.R
@@ -11,4 +11,9 @@ NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "n", "data",
-  "converged", "model_info", "log_lik", "df.null"))
+  "converged", "model_info", "log_lik", "df.null", "value", "value1", "prob",
+  "decile", "response", "total", "1", "0", "cum_total", "cum_1s", "cum_0s",
+  "cum_1s_%", "cum_0s_%", "tp", "tn", "fp", "fn", "sensitivity", "specificity",
+  "1 - specificity", "sensitivity_per", "no", "yes", "total", "dist_yes",
+  "dist_no", "dist_diff", "woe", "iv", "distribution", "approval", "gains_table",
+  "cum_total_per", "cum_1s_per", "cum_total_y"))
