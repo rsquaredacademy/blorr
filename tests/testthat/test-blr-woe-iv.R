ac0ed0ef44a86106c11c2a250d6051b2347495b3
rsquaredin <rsquaredin@gmail.com>
2018-01-04 20:15:02 +0530

    update tests

--- tests/testthat/test-blr-woe-iv.R
+++ tests/testthat/test-blr-woe-iv.R
@@ -3,6 +3,7 @@ context("test-blr-woe-iv.R")
 test_that('output from blr_woe_iv is as expected', {
 
   actual <- blr_woe_iv(hsb2, female, honcomp) %>%
+  	use_series(woe_iv_table) %>%
     pull(iv)
   expected <- c(0.06, 0.04)
   expect_equivalent(actual, expected)
