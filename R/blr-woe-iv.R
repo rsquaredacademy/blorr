bc736a3ee21b095ec3798a865fe921d169907a4c
rsquaredin <rsquaredin@gmail.com>
2018-01-11 12:53:50 +0530

    update print method

--- R/blr-woe-iv.R
+++ R/blr-woe-iv.R
@@ -55,7 +55,8 @@ blr_woe_iv.default <- function(data, predictor, response, digits = 2, ...) {
     ) %>%
     add_column(levels = lev, .before = 1) %>%
     select(-distribution, -approval) %>%
-    select(levels, `0` = no, `1` = yes, dist_1 = dist_yes, dist_0 = dist_no, woe = woe, iv = iv)
+    select(levels, `0s_count` = no, `1s_count` = yes, `0s_dist` = dist_no,
+           `1s_dist` = dist_yes, woe = woe, iv = iv)
 
   var_name <- dat %>%
     names %>%
@@ -70,9 +71,85 @@ blr_woe_iv.default <- function(data, predictor, response, digits = 2, ...) {
 #' @export
 #'
 print.blr_woe_iv <- function(x, ...) {
-  x %>%
+
+  y1 <- x %>%
     use_series(woe_iv_table) %>%
-    print
+    map(nchar) %>%
+    map_int(max)
+
+  y2 <- x %>%
+    use_series(woe_iv_table) %>%
+    names %>%
+    nchar
+
+  w <- map2_int(y1, y2, max)
+  wsum <- sum(w, 24)
+
+  rnames <- x %>%
+    use_series(woe_iv_table) %>%
+    names
+
+  woe_iv <- x %>%
+    use_series(woe_iv_table)
+
+  c1 <- woe_iv %>%
+    pull(rnames[1]) %>%
+    prepend(rnames[1])
+
+  c2 <- woe_iv %>%
+    pull(rnames[2]) %>%
+    prepend(rnames[2])
+
+  c3 <- woe_iv %>%
+    pull(rnames[3]) %>%
+    prepend(rnames[3])
+
+  c4 <- woe_iv %>%
+    pull(rnames[4]) %>%
+    prepend(rnames[4])
+
+  c5 <- woe_iv %>%
+    pull(rnames[5]) %>%
+    prepend(rnames[5])
+
+  c6 <- woe_iv %>%
+    pull(rnames[6]) %>%
+    prepend(rnames[6])
+
+  c7 <- woe_iv %>%
+    pull(rnames[7]) %>%
+    prepend(rnames[7])
+
+  cat(fc('Weight of Evidence', wsum), '\n')
+  cat(rep("-", wsum), sep = "", '\n')
+  for (i in seq_len(3)) {
+    cat(fc(c1[i], w[1]), fs(), fc(c2[i], w[2]), fs(),
+        fc(c3[i], w[3]), fs(), fc(c4[i], w[4]), fs(),
+        fc(c5[i], w[5]), fs(), fc(c6[i], w[6]), fs(),
+        fc(c7[i], w[7]), '\n')
+    if (i == 1) {
+      cat(rep("-", wsum), sep = "", '\n')
+    }
+  }
+  cat(rep("-", wsum), sep = "", '\n\n')
+
+  l1 <- c('Variable', x$var_name) %>%
+    nchar %>%
+    max
+  l2 <- 17
+  lsum <- sum(l1, l2, 4)
+
+  ivalue <- woe_iv %>%
+    pull(iv) %>%
+    sum
+
+  cat(fc('Information Value', lsum), '\n')
+  cat(rep("-", lsum), sep = "", '\n')
+  cat(fc('Variable', l1), fs(), fc('Information Value', l2), '\n')
+  cat(rep("-", lsum), sep = "", '\n')
+  cat(fc(x$var_name, l1), fs(), fc(ivalue, l2), '\n')
+  cat(rep("-", lsum), sep = "", '\n')
+
 }
 
 #' @export
