c327a3fdf3a9507c91bf8593139e282fec624466
rsquaredin <rsquaredin@gmail.com>
2018-01-15 17:34:45 +0530

    modify computation of pearson residual and deviance

--- R/blr-plots.R
+++ R/blr-plots.R
@@ -24,7 +24,7 @@ blr_plot_residual_fitted <- function(model, point_color = 'blue', line_color = '
     fitted
 
   res_val <- model %>%
-    residuals
+    rstandard(type = "pearson")
 
   tibble(fit = fit_val, resid = res_val) %>%
     ggplot() +
@@ -55,7 +55,7 @@ blr_plot_pearson_residual <- function(model, point_color = 'blue',
                                       yaxis_title = 'Standardized Pearson Residuals') {
 
   res_val <- model %>%
-    residuals
+    rstandard(type = 'pearson')
 
   id <- res_val %>%
     length %>%
@@ -65,5 +65,41 @@ blr_plot_pearson_residual <- function(model, point_color = 'blue',
     ggplot() +
     geom_point(aes(x = id, y = resid), color = point_color) +
     ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+
+}
+
+
+#' @title Deviance vs Fitted Values Plot
+#' @description Deviance vs fitted values plot
+#' @param model an object of class \code{glm}
+#' @param point_color color of the points
+#' @param line_color color of the horizontal line
+#' @param title title of the plot
+#' @param xaxis_title x axis label
+#' @param yaxis_title y axis label
+#' @examples
+#' model <- glm(honcomp ~ female + read + science, data = blorr::hsb2,
+#' family = binomial(link = 'logit'))
+#'
+#' blr_plot_deviance_fitted(model)
+#'
+#' @export
+#'
+blr_plot_deviance_fitted <- function(model, point_color = 'blue', line_color = 'red',
+                                     title = 'Deviance Residual vs Fitted Values',
+                                     xaxis_title = 'Fitted Values',
+                                     yaxis_title = 'Deviance Residual') {
+
+  fit_val <- model %>%
+    rstandard
+
+  res_val <- model %>%
+    residuals
+
+  tibble(fit = fit_val, resid = res_val) %>%
+    ggplot() +
+    geom_point(aes(x = fit, y = resid), color = point_color) +
+    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title) +
+    geom_hline(yintercept = 0, color = line_color)
 
 }
