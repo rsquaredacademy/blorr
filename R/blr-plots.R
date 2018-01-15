feaac4130bf4cf4e0c617ccac245d35328871b1e
rsquaredin <rsquaredin@gmail.com>
2018-01-15 17:16:48 +0530

    pearson standardized residuals plot

--- R/blr-plots.R
+++ R/blr-plots.R
@@ -34,4 +34,36 @@ blr_plot_residual_fitted <- function(model, point_color = 'blue', line_color = '
 
 }
 
+#' @title Residual Values Plot
+#' @description Standardised pearson residuals plot
+#' @param model an object of class \code{glm}
+#' @param point_color color of the points
+#' @param title title of the plot
+#' @param xaxis_title x axis label
+#' @param yaxis_title y axis label
+#' @examples
+#' model <- glm(honcomp ~ female + read + science, data = blorr::hsb2,
+#' family = binomial(link = 'logit'))
+#'
+#' blr_plot_pearson_residual(model)
+#'
+#' @export
+#'
+blr_plot_pearson_residual <- function(model, point_color = 'blue',
+                                      title = 'Standardized Pearson Residuals',
+                                      xaxis_title = 'id',
+                                      yaxis_title = 'Standardized Pearson Residuals') {
 
+  res_val <- model %>%
+    residuals
+
+  id <- res_val %>%
+    length %>%
+    seq_len
+
+  tibble(id = id, resid = res_val) %>%
+    ggplot() +
+    geom_point(aes(x = id, y = resid), color = point_color) +
+    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+
+}
