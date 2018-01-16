49c4c8d76a251fb8bd71e7c457fbcdbf8f1e7b5a
rsquaredin <rsquaredin@gmail.com>
2018-01-16 11:16:56 +0530

    add delta chi square vs fitted values plot

--- R/blr-plots.R
+++ R/blr-plots.R
@@ -513,3 +513,37 @@ blr_plot_c_fitted <- function(model, point_color = 'blue',
     ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
 
 }
+
+#' @title Delta Chi Square vs Fitted Values Plot
+#' @description Delta Chi Square vs fitted values plot for detecting ill fitted observations
+#' @param model an object of class \code{glm}
+#' @param point_color color of the points
+#' @param title title of the plot
+#' @param xaxis_title x axis label
+#' @param yaxis_title y axis label
+#' @examples
+#' model <- glm(honcomp ~ female + read + science, data = blorr::hsb2,
+#' family = binomial(link = 'logit'))
+#'
+#' blr_plot_difchisq_fitted(model)
+#'
+#' @export
+#'
+blr_plot_difchisq_fitted <- function(model, point_color = 'blue',
+                              title = 'Delta Chi Square vs Fitted Values Plot',
+                              xaxis_title = 'Fitted Values',
+                              yaxis_title = 'Delta Chi Square') {
+
+  res_val <- model %>%
+    blr_residual_diagnostics %>%
+    pull(difchisq)
+
+  fit_val <- model %>%
+    fitted.values
+
+  tibble(fit = fit_val, resid = res_val) %>%
+    ggplot() +
+    geom_point(aes(x = fit, y = resid), color = point_color) +
+    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+
+}
