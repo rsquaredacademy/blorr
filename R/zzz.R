4ca68b58d21e34f6005e058b5d02ca71656e03d4
rsquaredin <rsquaredin@gmail.com>
2018-01-20 17:44:38 +0530

    library load message

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onAttach <- function(...) {
+  if (!interactive() || stats::runif(1) > 0.1) return()
+
+  tips <- c(
+    "Learn more about blorr at http://github.com/rsquaredacademy/blorr/.",
+    "Use suppressPackageStartupMessages() to eliminate package startup messages.",
+    "Need help getting started with logisitc regression models? Visit: https://www.rsquaredacademy.com",
+    "Check out our interactive apps for quick data exploration. Visit: https://apps.rsquaredacademy.com/."
+  )
+
+  tip <- sample(tips, 1)
+  packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+}
