92b56defd2b1a5bfbd36e921b663bf83d8a9747b
rsquaredin <rsquaredin@gmail.com>
2018-01-24 21:02:31 +0530

    fixes #21

--- R/blr-woe-iv.R
+++ R/blr-woe-iv.R
@@ -1,5 +1,4 @@
 #' @importFrom rlang enquo !!
-#' @importFrom ggplot2 coord_flip
 #' @importFrom dplyr select rename
 #' @importFrom tibble add_column
 #' @title Weight of Evidence & Information Value
@@ -12,11 +11,17 @@
 #' @param title plot title
 #' @param xaxis_title x axis title
 #' @param yaxis_title y axis title
-#' @param bar_color color of the bar
+#' @param line_color color of the bar
+#' @param point_color color for the point
 #' @param ... other inputs
 #' @return a tibble
 #' @examples
-#' blr_woe_iv(hsb2, female, honcomp)
+#' # woe and iv
+#' k <- blr_woe_iv(hsb2, female, honcomp)
+#' k
+#'
+#' # plot woe
+#' plot(k)
 #' @export
 #'
 blr_woe_iv <- function(data, predictor, response, digits = 4, ...)
@@ -81,7 +86,7 @@ print.blr_woe_iv <- function(x, ...) {
 #'
 plot.blr_woe_iv <- function(x, title = NA, xaxis_title = 'Levels',
                             yaxis_title = 'WoE',
-                            bar_color = 'blue', ...) {
+                            line_color = 'blue', point_color = 'blue', ...) {
 
   if (is.na(title)) {
     plot_title <- x$var_name
@@ -92,8 +97,8 @@ plot.blr_woe_iv <- function(x, title = NA, xaxis_title = 'Levels',
   x %>%
     use_series(woe_iv_table) %>%
     ggplot() +
-    geom_col(aes(x = levels, y = woe), fill = bar_color) +
-    coord_flip() +
+    geom_line(aes(x = as.numeric(levels), y = woe), color = line_color) +
+    geom_point(aes(x = as.numeric(levels), y = woe), color = point_color) +
     ggtitle(plot_title) + xlab(xaxis_title) + ylab(yaxis_title)
 
 }
