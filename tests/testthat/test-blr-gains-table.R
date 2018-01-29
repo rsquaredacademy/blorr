8d173ca06c078f4b284e738be4bc50e31c79d954
rsquaredin <rsquaredin@gmail.com>
2018-01-29 12:27:05 +0530

    test #26 and #27

--- tests/testthat/test-blr-gains-table.R
+++ tests/testthat/test-blr-gains-table.R
@@ -1,9 +1,9 @@
 context("test-blr-gains-table.R")
 
-test_that("output from blr_gains_tableis as expected", {
+model <- glm(honcomp ~ female + read + science, data = hsb2,
+             family = binomial(link = 'logit'))
 
-  model <- glm(honcomp ~ female + read + science, data = blorr::hsb2,
-               family = binomial(link = 'logit'))
+test_that("output from blr_gains_table is as expected", {
 
   gtable <- blr_gains_table(model, hsb2)
   actual <- gtable %>%
@@ -15,5 +15,32 @@ test_that("output from blr_gains_tableis as expected", {
     round(2)
   expected <- c(80.94, 54.35, 61.40)
   expect_equivalent(actual, expected)
+
+})
+
+
+test_that("decile lift chart is as expected", {
+
+  skip_on_cran()
+
+  p <-
+    model %>%
+    blr_gains_table %>%
+    blr_decile_lift_chart
+
+  vdiffr::expect_doppelganger('decile lift chart', p$plot)
+
+})
+
+test_that("decile wise capture rate chart is as expected", {
+
+  skip_on_cran()
+
+  p <-
+    model %>%
+    blr_gains_table %>%
+    blr_decile_capture_rate
+
+  vdiffr::expect_doppelganger('decile capture rate chart', p$plot)
 
 })
