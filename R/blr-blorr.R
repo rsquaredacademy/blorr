152dcead342b3c99d27ca3cafde880ba1dbd3076
rsquaredin <rsquaredin@gmail.com>
2018-02-09 17:57:35 +0530

    Try editing in ess

--- R/blr-blorr.R
+++ R/blr-blorr.R
@@ -20,7 +20,7 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "n", "data",
   "response", "fit_val", '0s_expected', '0s_observed', '1s', '1s%', '1s_expected', '1s_observed', '2.5 %', '97.5 %',
   'ann_loc', 'ann_locate', 'avg_prob', 'cum_0s_per', 'cum_total_%', 'd_f', 'dist_table',
   'group', 'honcomp', 'ks', 'lr_ratio', 'n%', 'negative', 'p_value', 'positive', 'predict',
-  'predicted', 'prob_n', 'sec', 'test_result', 'variable', 'woe_iv_table', 'chisq_stat', 
+  'predicted', 'prob_n', 'sec', 'test_result', 'variable', 'woe_iv_table', 'chisq_stat',
   'confusion_matrix', 'partition_table', 'segment_data', 'var_name', 'fit', 'resid',
   'cbar', 'dev_df', 'difchisq', 'difdev', 'fitted', 'hat', 'lr_df', 'mfit',
   'twoway_segment', 'varnames'))
