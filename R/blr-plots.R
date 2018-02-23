ac57fac09b4904cb149b156c161de345480cae7b
rsquaredin <rsquaredin@gmail.com>
2018-02-23 16:09:59 +0530

    refactor plots

--- R/blr-plots.R
+++ R/blr-plots.R
@@ -20,17 +20,12 @@ blr_plot_residual_fitted <- function(model, point_color = "blue", line_color = "
                                      title = "Standardized Pearson Residual vs Fitted Values",
                                      xaxis_title = "Fitted Values",
                                      yaxis_title = "Standardized Pearson Residual") {
-  fit_val <- model %>%
-    fitted()
+  fit_val <- fitted(model)
+  res_val <- rstandard(model, type = "pearson")
 
-  res_val <- model %>%
-    rstandard(type = "pearson")
-
-  tibble(fit = fit_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = fit, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title) +
+  create_plot(fit_val, res_val, point_color, title, xaxis_title, yaxis_title) +
     geom_hline(yintercept = 0, color = line_color)
+
 }
 
 #' @title Residual Values Plot
@@ -52,17 +47,11 @@ blr_plot_pearson_residual <- function(model, point_color = "blue",
                                       title = "Standardized Pearson Residuals",
                                       xaxis_title = "id",
                                       yaxis_title = "Standardized Pearson Residuals") {
-  res_val <- model %>%
-    rstandard(type = "pearson")
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  res_val <- rstandard(model, type = "pearson")
+  id      <- plot_id(res_val)
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
 }
 
 
@@ -86,16 +75,11 @@ blr_plot_deviance_fitted <- function(model, point_color = "blue", line_color = "
                                      title = "Deviance Residual vs Fitted Values",
                                      xaxis_title = "Fitted Values",
                                      yaxis_title = "Deviance Residual") {
-  fit_val <- model %>%
-    fitted.values()
 
-  res_val <- model %>%
-    rstandard()
+  fit_val <- fitted(model)
+  res_val <- rstandard(model)
 
-  tibble(fit = fit_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = fit, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title) +
+  create_plot(fit_val, res_val, point_color, title, xaxis_title, yaxis_title) +
     geom_hline(yintercept = 0, color = line_color)
 }
 
@@ -118,17 +102,11 @@ blr_plot_deviance_residual <- function(model, point_color = "blue",
                                        title = "Deviance Residuals Plot",
                                        xaxis_title = "id",
                                        yaxis_title = "Deviance Residuals") {
-  res_val <- model %>%
-    rstandard()
+  res_val <- rstandard(model)
+  id      <- plot_id(res_val)
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
 }
 
 
@@ -151,16 +129,12 @@ blr_plot_leverage_fitted <- function(model, point_color = "blue",
                                      title = "Leverage vs Fitted Values",
                                      xaxis_title = "Fitted Values",
                                      yaxis_title = "Leverage") {
-  fit_val <- model %>%
-    fitted.values()
 
-  res_val <- model %>%
-    hatvalues()
+  fit_val <- fitted(model)
+  res_val <- hatvalues(model)
 
-  tibble(fit = fit_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = fit, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(fit_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 
@@ -183,17 +157,12 @@ blr_plot_leverage <- function(model, point_color = "blue",
                               title = "Leverage Plot",
                               xaxis_title = "id",
                               yaxis_title = "Leverage") {
-  res_val <- model %>%
-    hatvalues()
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  res_val <- hatvalues(model)
+  id      <- plot_id(res_val)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 
@@ -211,35 +180,27 @@ blr_plot_leverage <- function(model, point_color = "blue",
 #' @export
 #'
 blr_residual_diagnostics <- function(model) {
-  hat_val <- model %>%
-    hatvalues()
 
-  res_val <- model %>%
+  res_val <-
+    model %>%
     residuals(type = "pearson") %>%
     raise_to_power(2)
 
-  num <- res_val * hat_val
+  hat_val  <- hatvalues(model)
+  num      <- res_val * hat_val
+  den      <- 1 - hat_val
+  c        <- num / (den ^ 2)
+  cbar     <- num / den
+  difchisq <- cbar / hat_val
 
-  den <- 1 %>%
-    subtract(hat_val)
-
-  c <- num %>%
-    divide_by(den %>%
-      raise_to_power(2))
-
-  cbar <- num %>%
-    divide_by(den)
-
-  difdev <- model %>%
+  difdev <-
+    model %>%
     rstandard() %>%
     raise_to_power(2) %>%
     add(cbar)
 
-  difchisq <- cbar %>%
-    divide_by(hat_val)
+  tibble(c = c, cbar = cbar, difdev = difdev, difchisq = difchisq)
 
-  result <- tibble(c = c, cbar = cbar, difdev = difdev, difchisq = difchisq)
-  return(result)
 }
 
 
@@ -262,18 +223,12 @@ blr_plot_diag_c <- function(model, point_color = "blue",
                             title = "CI Displacement C Plot",
                             xaxis_title = "id",
                             yaxis_title = "CI Displacement C") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(c)
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  res_val <- extract_diag(model, c)
+  id      <- plot_id(res_val)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 
@@ -296,18 +251,12 @@ blr_plot_diag_cbar <- function(model, point_color = "blue",
                                title = "CI Displacement CBAR Plot",
                                xaxis_title = "id",
                                yaxis_title = "CI Displacement CBAR") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(cbar)
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  res_val <- extract_diag(model, cbar)
+  id      <- plot_id(res_val)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 
@@ -330,18 +279,12 @@ blr_plot_diag_difchisq <- function(model, point_color = "blue",
                                    title = "Delta Chisquare Plot",
                                    xaxis_title = "id",
                                    yaxis_title = "Delta Chisquare") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(difchisq)
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  res_val <- extract_diag(difchisq)
+  id      <- plot_id(res_val)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 #' @title Delta Deviance Plot
@@ -363,18 +306,12 @@ blr_plot_diag_difdev <- function(model, point_color = "blue",
                                  title = "Delta Deviance Plot",
                                  xaxis_title = "id",
                                  yaxis_title = "Delta Deviance") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(difdev)
 
-  id <- res_val %>%
-    length() %>%
-    seq_len()
+  res_val <- extract_diag(difdev)
+  id      <- plot_id(res_val)
 
-  tibble(id = id, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = id, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(id, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 
@@ -406,58 +343,34 @@ blr_plot_diag_difdev <- function(model, point_color = "blue",
 #' @export
 #'
 blr_plot_dfbetas_panel <- function(model) {
+
   if (!any(class(model) == "glm")) {
     stop("Please specify a binary logistic regression model.", call. = FALSE)
   }
 
-  dfb <- dfbetas(model)
-  n <- nrow(dfb)
-  np <- ncol(dfb)
+  dfb       <- dfbetas(model)
+  n         <- nrow(dfb)
+  np        <- ncol(dfb)
   threshold <- 2 / sqrt(n)
-  obs <- NULL
-  txt <- NULL
-  Observation <- NULL
-  myplots <- list()
-  outliers <- list()
-  for (i in seq_len(np)) {
-    dbetas <- dfb[, i]
+  myplots   <- list()
+  outliers  <- list()
 
-    d <- tibble(obs = seq_len(n), dbetas = dbetas)
-    d$color <- ifelse(((d$dbetas >= threshold) | (d$dbetas <= -threshold)), c("outlier"), c("normal"))
-    d$color1 <- factor(d$color)
-    d$Observation <- ordered(d$color1, levels = c("normal", "outlier"))
-    d <- d %>% mutate(txt = ifelse(Observation == "outlier", obs, NA))
-    f <- d %>%
-      filter(., Observation == "outlier") %>%
-      select(obs, dbetas)
-    p <- eval(substitute(
-      ggplot(d, aes(x = obs, y = dbetas, label = txt, ymin = 0, ymax = dbetas)) +
-        geom_linerange(colour = "blue") +
-        geom_hline(yintercept = c(0, threshold, -threshold), colour = "red") +
-        geom_point(colour = "blue", shape = 1) +
-        xlab("Observation") + ylab("DFBETAS") +
-        ggtitle(paste("Influence Diagnostics for", colnames(dfb)[i])) +
-        geom_text(hjust = -0.2, nudge_x = 0.15, size = 2, family = "serif", fontface = "italic", colour = "darkred", na.rm = TRUE) +
-        annotate(
-          "text", x = Inf, y = Inf, hjust = 1.5, vjust = 2,
-          family = "serif", fontface = "italic", colour = "darkred",
-          label = paste("Threshold:", round(threshold, 2))
-        ),
-      list(i = i)
-    ))
-    # print(p)
-    myplots[[i]] <- p
+  for (i in seq_len(np)) {
+
+    d <- dfbetas_data_prep(dfb, n, i)
+    f <- dfbetas_outlier_data(d)
+    p <- eval(substitute(dfbetas_plot(d, threshold, dfb, i),list(i = i)))
+
+    myplots[[i]]  <- p
     outliers[[i]] <- f
+
   }
 
   suppressWarnings(do.call(grid.arrange, c(myplots, list(ncol = 2))))
-
-  names(outliers) <- model %>%
-    coefficients() %>%
-    names()
-
+  names(outliers) <- model_coeff_names(model)
   result <- list(outliers = outliers, plots = myplots)
   invisible(result)
+
 }
 
 #' @title CI Displacement C vs Fitted Values Plot
@@ -479,17 +392,12 @@ blr_plot_c_fitted <- function(model, point_color = "blue",
                               title = "CI Displacement C vs Fitted Values Plot",
                               xaxis_title = "Fitted Values",
                               yaxis_title = "CI Displacement C") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(c)
 
-  fit_val <- model %>%
-    fitted.values()
+  res_val <- extract_diag(model, c)
+  fit_val <- fitted(model)
 
-  tibble(fit = fit_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = fit, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(fit_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 #' @title Delta Chi Square vs Fitted Values Plot
@@ -511,17 +419,12 @@ blr_plot_difchisq_fitted <- function(model, point_color = "blue",
                                      title = "Delta Chi Square vs Fitted Values Plot",
                                      xaxis_title = "Fitted Values",
                                      yaxis_title = "Delta Chi Square") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(difchisq)
 
-  fit_val <- model %>%
-    fitted.values()
+  res_val <- extract_diag(difchisq)
+  fit_val <- fitted(model)
 
-  tibble(fit = fit_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = fit, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(fit_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 #' @title Delta Deviance vs Fitted Values Plot
@@ -543,17 +446,12 @@ blr_plot_difdev_fitted <- function(model, point_color = "blue",
                                    title = "Delta Deviance vs Fitted Values Plot",
                                    xaxis_title = "Fitted Values",
                                    yaxis_title = "Delta Deviance") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(difdev)
 
-  fit_val <- model %>%
-    fitted.values()
+  res_val <- extract_diag(model, difdev)
+  fit_val <- fitted(model)
 
-  tibble(fit = fit_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = fit, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(fit_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 #' @title Delta Deviance vs Leverage Plot
@@ -575,17 +473,12 @@ blr_plot_difdev_leverage <- function(model, point_color = "blue",
                                      title = "Delta Deviance vs Leverage Plot",
                                      xaxis_title = "Leverage",
                                      yaxis_title = "Delta Deviance") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(difdev)
 
-  hat_val <- model %>%
-    hatvalues()
+  res_val <- extract_diag(model, difdev)
+  hat_val <- hatvalues(model)
 
-  tibble(hat = hat_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = hat, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(hat_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 #' @title Delta Chi Square vs Leverage Plot
@@ -607,17 +500,12 @@ blr_plot_difchisq_leverage <- function(model, point_color = "blue",
                                        title = "Delta Chi Square vs Leverage Plot",
                                        xaxis_title = "Leverage",
                                        yaxis_title = "Delta Chi Square") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(difchisq)
 
-  hat_val <- model %>%
-    hatvalues()
+  res_val <- extract_diag(model, difchisq)
+  hat_val <- hatvalues(model)
 
-  tibble(hat = hat_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = hat, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(hat_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 #' @title CI Displacement C vs Leverage Plot
@@ -639,17 +527,12 @@ blr_plot_c_leverage <- function(model, point_color = "blue",
                                 title = "CI Displacement C vs Leverage Plot",
                                 xaxis_title = "Leverage",
                                 yaxis_title = "CI Displacement C") {
-  res_val <- model %>%
-    blr_residual_diagnostics() %>%
-    pull(c)
 
-  hat_val <- model %>%
-    hatvalues()
+  res_val <- extract_diag(model, c)
+  hat_val <- hatvalues(model)
 
-  tibble(hat = hat_val, resid = res_val) %>%
-    ggplot() +
-    geom_point(aes(x = hat, y = resid), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(hat_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
 }
 
 
@@ -672,14 +555,90 @@ blr_plot_fitted_leverage <- function(model, point_color = "blue",
                                      title = "Fitted Values vs Leverage Plot",
                                      xaxis_title = "Leverage",
                                      yaxis_title = "Fitted Values") {
-  fit_val <- model %>%
-    fitted.values()
 
-  hat_val <- model %>%
-    hatvalues()
+  fit_val <- fitted(model)
+  hat_val <- hatvalues(model)
 
-  tibble(hat = hat_val, fit = fit_val) %>%
-    ggplot() +
-    geom_point(aes(x = hat, y = fit), color = point_color) +
-    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+  create_plot(hat_val, res_val, point_color, title, xaxis_title, yaxis_title)
+
+
 }
+
+plot_id <- function(res_val) {
+
+  res_val %>%
+    length() %>%
+    seq_len()
+
+}
+
+
+extract_diag <- function(model, value) {
+
+  vals <- enquo(value)
+
+  model %>%
+    blr_residual_diagnostics() %>%
+    pull(!! vals)
+
+}
+
+
+dfbetas_data_prep <- function(dfb, n, i) {
+
+  dbetas <- dfb[, i]
+  tibble(obs = seq_len(n), dbetas = dbetas) %>%
+    mutate(
+      color = ifelse(((dbetas >= threshold) | (dbetas <= -threshold)),
+                      c("outlier"), c("normal")),
+      fct_color = color %>%
+        factor() %>%
+        ordered(levels = c("normal", "outlier")),
+      txt = ifelse(color == "outlier", obs, NA)
+    )
+
+}
+
+dfbetas_plot <- function(d, threshold, dfb, i) {
+
+  ggplot(d, aes(x = obs, y = dbetas, label = txt, ymin = 0, ymax = dbetas)) +
+    geom_linerange(colour = "blue") +
+    geom_hline(yintercept = c(0, threshold, -threshold), colour = "red") +
+    geom_point(colour = "blue", shape = 1) +
+    xlab("Observation") + ylab("DFBETAS") +
+    ggtitle(paste("Influence Diagnostics for", colnames(dfb)[i])) +
+    geom_text(hjust = -0.2, nudge_x = 0.15, size = 2, family = "serif",
+              fontface = "italic", colour = "darkred", na.rm = TRUE) +
+    annotate(
+      "text", x = Inf, y = Inf, hjust = 1.5, vjust = 2,
+      family = "serif", fontface = "italic", colour = "darkred",
+      label = paste("Threshold:", round(threshold, 2))
+    )
+}
+
+
+dfbetas_outlier_data <- function(d) {
+
+  d %>%
+    filter(color == "outlier") %>%
+    select(obs, dbetas)
+}
+
+
+model_coeff_names <- function(model) {
+
+  model %>%
+    coefficients() %>%
+    names()
+}
+
+create_plot <- function(x, y, point_color, title, xaxis_title, yaxis_title) {
+
+  tibble(x = x, y = y) %>%
+    ggplot() +
+    geom_point(aes(x = x, y = y), color = point_color) +
+    ggtitle(title) + xlab(xaxis_title) + ylab(yaxis_title)
+
+}
+
+
