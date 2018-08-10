cb216b8365a390463d2f444fdf572e7823635ac4
rsquaredin <rsquaredin@gmail.com>
2018-08-10 10:15:58 +0530

    update website

--- inst/application/ui.R
+++ inst/application/ui.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 shinyUI(
   
-navbarPage(HTML("blorr"), id = 'mainpage',
+navbarPage(HTML("blorr - Build Logistic Regression Models"), id = 'mainpage',
              
   source('ui/ui_data.R', local = TRUE)[[1]],
   source('ui/ui_analyze.R', local = TRUE)[[1]],
