7e8029dcbdde41426cfed0f3dfd5f9bee3fa1b2f
aravindhebbali <hebbali.aravind@gmail.com>
2024-11-11 15:16:32 +0530

    test: remove snapshots:

--- tests/testthat/test-stepwise-p.R
+++
@@ -1,7 +0,0 @@
-test_that("output from forward variable selection is as expected", {
-
-  model <- glm(y ~ ., data = stepwise, family = binomial(link = 'logit'))
-  expect_snapshot(blr_step_p_both(model))
-
-})
-
